d59af22bf373b082fc9ab67c8ed2e7f109a3ebdb
Swarup Vishwas <91014156+SwarupVishwas18@users.noreply.github.com>
2022-10-17 13:09:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ document.getElementById('whatsapp').addEventListener('click', function(){
 });
 
 document.getElementById('download').addEventListener('click', function(){
-    window.open('./download.html', '_blank');
+    window.open('/anime-quotify/download.html', '_blank');
 })
 
 // NAVBAR
