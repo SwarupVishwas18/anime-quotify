08191006cd3c7d3578f9a664d4ec89a44268c583
Swarup Vishwas <swarupvishwas000@gmail.com>
2024-02-05 22:59:58 +0530

    Change API URL

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 
 /*  PART IV : SAVE THE CURRENT QUOTE  */
 
-var getQuote = function(){
-    if(localStorage.getItem('quote')){
+var getQuote = function () {
+    if (localStorage.getItem('quote')) {
         quoteCont.textContent = localStorage.getItem('quote');
         nameCont.textContent = localStorage.getItem('name');
         charCont.textContent = localStorage.getItem('char');
@@ -10,55 +10,55 @@ var getQuote = function(){
     } else {
         return false;
     }
-    }
+}
 
 /*  PART I : GENERATE ON LOAD  */
 
 const colors = [{
-    bgCol : "#76cbe4",
-    textCol : "#320b2d",
+    bgCol: "#76cbe4",
+    textCol: "#320b2d",
     btnBg: "#c73b4a",
     btnCol: "#fcfefe"
 },
 {
-    bgCol : "#22d3a9",
-    textCol : "#231e20",
+    bgCol: "#22d3a9",
+    textCol: "#231e20",
     btnBg: "#444f45",
     btnCol: "#fffefd"
 },
 {
-    bgCol : "#22d3a9",
-    textCol : "#191819",
+    bgCol: "#22d3a9",
+    textCol: "#191819",
     btnBg: "#4e565f",
     btnCol: "#eff3f8"
 },
 {
-    bgCol : "#ff6f7c",
-    textCol : "#231e20",
+    bgCol: "#ff6f7c",
+    textCol: "#231e20",
     btnBg: "#70a4be",
     btnCol: "#f0efee"
 },
 {
-    bgCol : "#fbf4db",
-    textCol : "#231e20",
+    bgCol: "#fbf4db",
+    textCol: "#231e20",
     btnBg: "#c84d61",
     btnCol: "#fbf4db"
 },
 {
-    bgCol : "#94bdbb",
-    textCol : "#231e20",
+    bgCol: "#94bdbb",
+    textCol: "#231e20",
     btnBg: "#f36a4b",
     btnCol: "#fbffff"
 },
 {
-    bgCol : "#dca9a6",
-    textCol : "#231e20",
+    bgCol: "#dca9a6",
+    textCol: "#231e20",
     btnBg: "#7095dd",
     btnCol: "#fdffff"
 },
 {
-    bgCol : "#4ebfda",
-    textCol : "#231e20",
+    bgCol: "#4ebfda",
+    textCol: "#231e20",
     btnBg: "#f4cf0e",
     btnCol: "#231e20"
 }
@@ -71,7 +71,7 @@ var nameCont = document.querySelector('#name-anime');
 var charCont = document.querySelector('#name-char');
 // Create a function for setting a variable value
 function myFunction_set() {
-    var currentCol = colors[parseInt(Math.random()*8)];
+    var currentCol = colors[parseInt(Math.random() * 8)];
     var bgCol = currentCol.bgCol;
     var textCol = currentCol.textCol;
     var btnBg = currentCol.btnBg;
@@ -82,24 +82,24 @@ function myFunction_set() {
     r.style.setProperty('--btnCol', btnCol);
 
     var label = getQuote();
-    if(!label){
+    if (!label) {
 
-    fetch('https://animechan.vercel.app/api/random')
-    .then(response => response.json())
-    .then( quote => {
-        console.log(quote);
-        quoteCont.textContent = quote.quote;
-        nameCont.textContent = quote.anime;
-        charCont.textContent = quote.character;
-        localStorage.setItem('quote', quoteCont.textContent);
-        localStorage.setItem('name', nameCont.textContent);
-        localStorage.setItem('char', charCont.textContent);
-    
-    });
-}
+        fetch('https://animechan.vercel.app/api/random')
+            .then(response => response.json())
+            .then(quote => {
+                console.log(quote);
+                quoteCont.textContent = quote.quote;
+                nameCont.textContent = quote.anime;
+                charCont.textContent = quote.character;
+                localStorage.setItem('quote', quoteCont.textContent);
+                localStorage.setItem('name', nameCont.textContent);
+                localStorage.setItem('char', charCont.textContent);
 
-    
-   
+            });
+    }
+
+
+
 }
 
 
@@ -112,23 +112,23 @@ var exitFscreen = document.querySelector('#fscreen-exit');
 var gen = document.querySelector('#generate');
 var share = document.querySelector('#share');
 var fscreen = document.querySelector('#fscreen');
-fscreen.addEventListener('click', function(){
+fscreen.addEventListener('click', function () {
     if (elem.requestFullscreen) {
         myFunction_set()
         elem.requestFullscreen();
-      } else if (elem.webkitRequestFullscreen) { /* Safari */
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
         elem.webkitRequestFullscreen();
-      } else if (elem.msRequestFullscreen) { /* IE11 */
+    } else if (elem.msRequestFullscreen) { /* IE11 */
         elem.msRequestFullscreen();
-      }
-      fscreen.style.display = 'none';
-      gen.style.display = 'none';
-      share.style.display = 'none';
-      exitFscreen.style.display = 'inline';
+    }
+    fscreen.style.display = 'none';
+    gen.style.display = 'none';
+    share.style.display = 'none';
+    exitFscreen.style.display = 'inline';
 
 });
 
-exitFscreen.addEventListener('click', function(){
+exitFscreen.addEventListener('click', function () {
     fscreen.style.display = 'inline';
     exitFscreen.style.display = 'none';
     gen.style.display = 'inline';
@@ -138,7 +138,7 @@ exitFscreen.addEventListener('click', function(){
 
 /*  PART III : COPY TO CLIP  */
 
-document.querySelector('#clip').addEventListener('click', function(){
+document.querySelector('#clip').addEventListener('click', function () {
     navigator.clipboard.writeText(document.querySelector('#quote').textContent);
     alert('Quote Copied Successfully..!!')
 })
@@ -146,8 +146,8 @@ document.querySelector('#clip').addEventListener('click', function(){
 /*  PART IV : GENERATE ON CLICK  */
 var gen = document.getElementById('generate');
 
-gen.addEventListener('click', function(){
-    var currentCol = colors[parseInt(Math.random()*8)];
+gen.addEventListener('click', function () {
+    var currentCol = colors[parseInt(Math.random() * 8)];
     var bgCol = currentCol.bgCol;
     var textCol = currentCol.textCol;
     var btnBg = currentCol.btnBg;
@@ -157,40 +157,40 @@ gen.addEventListener('click', function(){
     r.style.setProperty('--btnBg', btnBg);
     r.style.setProperty('--btnCol', btnCol);
     var flag = true;
-    
-    fetch('https://animechan.vercel.app/api/random')
-    .then(response => response.json())
-    .then( quote => {
-        console.log(quote);
-        quoteCont.textContent = quote.quote;
-        nameCont.textContent = quote.anime;
-        charCont.textContent = quote.character;
-        localStorage.setItem('quote', quoteCont.textContent);
-        localStorage.setItem('name', nameCont.textContent);
-        localStorage.setItem('char', charCont.textContent);
-     
-    });
+
+    fetch('https://animechan.xyz/api/random')
+        .then(response => response.json())
+        .then(quote => {
+            console.log(quote);
+            quoteCont.textContent = quote.quote;
+            nameCont.textContent = quote.anime;
+            charCont.textContent = quote.character;
+            localStorage.setItem('quote', quoteCont.textContent);
+            localStorage.setItem('name', nameCont.textContent);
+            localStorage.setItem('char', charCont.textContent);
+
+        });
 });
 
-document.querySelector('#share').addEventListener('click', function(){
+document.querySelector('#share').addEventListener('click', function () {
     modal.style.display = 'flex';
 });
 
-document.querySelector('#exit-modal').addEventListener('click', function(){
+document.querySelector('#exit-modal').addEventListener('click', function () {
     modal.style.display = 'none';
 });
 
 // Whatsapp Sharing
 
-document.getElementById('whatsapp').addEventListener('click', function(){
-    var text = "*"+document.querySelector('#quote').textContent + "* " + '\n\nCreated By Quotify';
+document.getElementById('whatsapp').addEventListener('click', function () {
+    var text = "*" + document.querySelector('#quote').textContent + "* " + '\n\nCreated By Quotify';
     var link = `https://wa.me?text=${text}`;
     // alert('click to ho raha hain');
     window.open(link, '_blank');
 });
 
-document.getElementById('download').addEventListener('click', function(){
-    window.open('/anime-quotify/download.html', '_blank');
+document.getElementById('download').addEventListener('click', function () {
+    window.open('/download.html', '_blank');
 })
 
 // NAVBAR
@@ -198,24 +198,24 @@ document.getElementById('download').addEventListener('click', function(){
 var lines = document.querySelector('#lines');
 var links = document.querySelector('#nav-links');
 var x = document.querySelector('#x');
-lines.addEventListener('click', function(){
-    if(links.style.display == 'none'){
+lines.addEventListener('click', function () {
+    if (links.style.display == 'none') {
         links.style.display = 'flex';
         x.style.display = 'block';
         lines.style.display = 'none';
-    }else{
+    } else {
         links.style.display = 'none';
         x.style.display = 'none';
         lines.style.display = 'flex';
     }
 });
 
-x.addEventListener('click', function(){
-    if(links.style.display == 'none'){
+x.addEventListener('click', function () {
+    if (links.style.display == 'none') {
         links.style.display = 'flex';
         x.style.display = 'block';
         lines.style.display = 'none';
-    }else{
+    } else {
         links.style.display = 'none';
         x.style.display = 'none';
         lines.style.display = 'flex';
